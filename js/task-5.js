18c99962c340fabf9cb408a2fe16c2de78aea6f8
Oleksii <shcherbaalex77@gmail.com>
2024-03-03 14:14:18 -0500

    finish task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body_element = document.querySelector('body');
+const button_element = document.querySelector('.change-color');
+const color_element = document.querySelector('.color');
+
+button_element.addEventListener('click', event => {
+  body_element.style.backgroundColor = getRandomHexColor();
+  color_element.textContent = body_element.style.backgroundColor;
+});
