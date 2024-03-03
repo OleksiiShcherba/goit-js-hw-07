62a1421f9495fc49dc2c9fc9ff617da2a168c93a
Oleksii <shcherbaalex77@gmail.com>
2024-03-03 13:49:32 -0500

    finished task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const form_element = document.querySelector('form.login-form');
+const submit_action = event => {
+    event.preventDefault();
+    
+    const result = {};
+    let successfully = true;
+    for (let form_data_element of event.target.elements) {
+        if (form_data_element.value.trim().length > 0) {
+            result[form_data_element.name] = form_data_element.value.trim();
+        } else if (['password', 'email'].includes(form_data_element.name)) {
+            alert('All form fields must be filled in');
+            successfully = false;
+            break;
+        }
+    }
+    
+    if (successfully) {
+        event.target.reset();
+        console.log(result);
+    }
+};
 
+form_element.addEventListener('submit', submit_action);
